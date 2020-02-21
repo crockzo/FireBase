3c0ca3d42a81684744999dfcb79bc142e7228852
mohit <guptamrmohit08@gmail.com>
2020-02-21 22:34:46 +0530

    inserting and reading data from the root node of the database

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,14 +1,70 @@
 package com.example.firebase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private FirebaseDatabase mfirebaseDatabase;
+    private DatabaseReference mRef;
+    private Button mButton , readData;
+    private TextView setData;
+    private EditText mtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mfirebaseDatabase = FirebaseDatabase.getInstance();
+        mRef = mfirebaseDatabase.getReference();
+
+       init();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String data = mtext.getText().toString();
+                mRef.setValue(data);
+                Toast.makeText(MainActivity.this, "data inserted " , Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+        readData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mRef.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            String data = dataSnapshot.getValue(String.class);
+                            setData.setText(data);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    public void init(){
+        mtext = findViewById(R.id.setData);
+        mButton= findViewById(R.id.sendData);
+        readData = findViewById(R.id.getDataFromDataBase);
+        setData = findViewById(R.id.setReadData);
     }
 }
