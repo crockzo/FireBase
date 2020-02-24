138937aa792cc3b0ea58fe5aae1ef0fb4d12c9c7
mohit <guptamrmohit08@gmail.com>
2020-02-24 19:14:44 +0530

    added multiple value in one command --- added a childViewListener -- use map within map ---

--- app/src/main/java/com/example/firebase/MainActivity.java
+++ app/src/main/java/com/example/firebase/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.firebase;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -13,19 +14,22 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseDatabase mfirebaseDatabase;
     private DatabaseReference mRef;
-    private Button mButton , readData;
+    private Button mWriteData, readData , mUpdate ,mdelete;
     private TextView setData;
     private EditText mtext , mAge;
     private String TAG = "mytag";
@@ -49,39 +53,38 @@ public class MainActivity extends AppCompatActivity {
 
        init();
 
-        mButton.setOnClickListener(new View.OnClickListener() {
+
+
+        mWriteData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String data = mtext.getText().toString();
-
-//                TO CREATE THE FURTHER CHILD NODE
+               final  String data = mtext.getText().toString();
+                final String age = mAge.getText().toString();
 //                mRef.child("user!").setValue(data);
-
+                Map<String,Object> map = new HashMap<>();
+                map.put("name",data);
+                map.put("age",age);
 
                 String key = mRef.push().getKey().toString();
-//                THIS IS TO SET THE VALUE OF TO THE RELATED VALUE  : HERE THE mRef REFERES TO THE NODE
-                mRef.child(key).child("name").setValue(data)
-
-//                        THIS IS TO KNOW THAT THE PROCESS IS SUCCESSFUL OR NOT
+                mRef.child(key).setValue(map);
+              /*  mRef.child(key).child("name").setValue(data)
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
                                 Toast.makeText(MainActivity.this, "write successfully", Toast.LENGTH_SHORT).show();
-                                Log.d(TAG, "onSuccess: "+"dataloaded");
+                                Log.d(TAG, "onSuccess: name : " + data);
                             }
                         })
-
-//                        THIS OS TO KNOW THAT THE PROCESS IS FAILED
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
                                 Log.d(TAG, "onFailure: permissiion denied");
                             }
-                        });
-
-                String age = mAge.getText().toString();
+                        });*/
+//   mRef.child("age").setValue(age)
+/*
+               final String age = mAge.getText().toString();
                 mRef.child(key).child("age").setValue(age)
-             //   mRef.child("age").setValue(age)
                         .addOnFailureListener(new OnFailureListener() {
                             @Override
                             public void onFailure(@NonNull Exception e) {
@@ -91,11 +94,11 @@ public class MainActivity extends AppCompatActivity {
                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void aVoid) {
-                                Log.d(TAG, "onSuccess: age add successfully");
+                                Log.d(TAG, "onSuccess: age :" + age);
                             }
                         });
                // mRef.setValue(data);
-                Toast.makeText(MainActivity.this, "data inserted " , Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "data inserted " , Toast.LENGTH_LONG).show();*/
             }
         });
 
@@ -104,42 +107,112 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+/*
 //                THIS EVENT LISTENER ID USE TO REFERENCE TO A NODE AND SHOW THE VALUE OF THE NODE
                 mValuelistener = new ValueEventListener() {
                     @Override
                     public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                        DATA SNAPSHOT GIVE THE SNAPSHOT OF THE CURRENT NODE TO ACCESS THE VALUE ATTACHED TO THE NODE
-                        Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                        for(DataSnapshot snapshot : dataSnapshot.getChildren()){
 
-                       // String data = dataSnapshot.getValue(String.class);
-                        String data = map.get("name").toString();
+                             try {
+                                 Map<String,Object> map = (Map<String, Object>) snapshot.getValue();
+                                Log.d(TAG, "onDataChange: key " + snapshot.getKey() );
+                                Log.d(TAG, "onDataChange: name " + map.get("name"));
+                                Log.d(TAG, "onDataChange: age" + map.get("age"));
+                            }catch (Exception e){
+                                Log.d(TAG, "onDataChange: error found");
+                            }
+
+
+                        }
+*/
+
+                       /*
+                        Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                         String data = map.get("name").toString();
                         setData.setText(data);
                         Log.d(TAG, "onDataChange: " + data);
+                        */
 
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Log.d(TAG, "onCancelled: failed in reading");
-
-                    }
-                };
-//                THIS IS TO SET THE EVENT LISTENER WHICH GET INVOKED WHEN THE DATA OF THE NODE IS GET UPDATED OR CHANGES
-                mRef.child("user").addValueEventListener(mValuelistener);
-                /*(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        DATA SNAPSHOT GIVE THE SNAPSHOT OF THE CURRENT NODE TO ACCESS THE VALUE ATTACHED TO THE NODE
-                            String data = dataSnapshot.getValue(String.class);
-                            setData.setText(data);
-                        Log.d(TAG, "onDataChange: " + data);
-                    }
+                   /* }
 
                     @Override
                     public void onCancelled(@NonNull DatabaseError databaseError) {
                         Log.d(TAG, "onCancelled: failed in reading");
                     }
-                });*/
+                };*/
+            //    mRef.addListenerForSingleValueEvent(mValuelistener);
+                mRef.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                        try {
+                            Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                            Log.d(TAG, "onChildAdded: key " + dataSnapshot.getKey() );
+                            Log.d(TAG, "onChildAdded: name " + map.get("name"));
+                            Log.d(TAG, "onChildAdded: age" + map.get("age"));
+                        }catch (Exception e){
+                            Log.d(TAG, "onChildAdded: error found");
+                        }
+                    }
+
+                    @Override
+                    public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                      /*  try {
+                            Map<String,Object> map = (Map<String, Object>) dataSnapshot.getValue();
+                            Log.d(TAG, "onDataChange: key " + dataSnapshot.getKey() );
+                            Log.d(TAG, "onDataChange: name " + map.get("name"));
+                            Log.d(TAG, "onDataChange: age" + map.get("age"));
+                        }catch (Exception e){
+                            Log.d(TAG, "onChildChange: error found");
+                        }*/
+                    }
+
+                    @Override
+                    public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        mUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Map<String,Object> mapUpdate = new HashMap<>() ;
+//                mapUpdate.put("/-M0mCIPlEMgBxvn8f0s1/name","vishan kumar gupta");
+//                mapUpdate.put("/-M0mCIPlEMgBxvn8f0s1/age",47);
+                mapUpdate.put("/user1/name","ram");
+                mapUpdate.put("/user1/age","19");
+                mRef.updateChildren(mapUpdate);
+            }
+        });
+
+        mdelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Task<Void> task =  mRef.child("user1").child("name").removeValue();
+                task.addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "onSuccess: data deleted");
+                    }
+                })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.d(TAG, "onFailure: dalete cancel");
+                            }
+                        });
             }
         });
     }
@@ -152,9 +225,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void init(){
         mtext = findViewById(R.id.setData);
-        mButton= findViewById(R.id.sendData);
+        mWriteData = findViewById(R.id.sendData);
         readData = findViewById(R.id.getDataFromDataBase);
         setData = findViewById(R.id.setReadData);
         mAge = findViewById(R.id.setAge);
+        mUpdate = findViewById(R.id.update);
+        mdelete = findViewById(R.id.delete);
     }
 }
